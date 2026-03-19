4b2a99c1548347310624308d724590260043f671
momoirotj <8408219+momoirotj@users.noreply.github.com>
2026-03-20 07:26:31 +0900

    merge branch feature/button2

--- source/feature4/main.c
+++ source/feature4/main.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
+#include <stdint.h>
 
 int main(int argc, char** argv){
-    printf("Hello World.\n");
+    printf("Hello World. this feature is button1.\n");
     return 0;
 }
 
