0644206af0a668bf2a6e4667c85d9e3dec99dfd3
momoirotj <8408219+momoirotj@users.noreply.github.com>
2026-07-05 15:26:12 +0900

    コメントの間違いを修正

--- source/feature7/main.c
+++ source/feature7/main.c
@@ -2,7 +2,7 @@
 #include <math.h>
 
 int main(int argc, char** argv){
-    printf("This is feature6. feature7 is button4.\n");
+    printf("This is feature7. feature7 is button4.\n");
     return 0;
 }
 
